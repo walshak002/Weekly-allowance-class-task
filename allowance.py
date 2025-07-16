67c4f432c23cc716469a7d991680ee7347de1e67
walshak123 <patrickwalshak1@gmail.com>
2025-07-16 18:43:32 +0100

    Created a weekly allowance income

---
+++ allowance.py
@@ -0,0 +1,26 @@
+print("My weekly Allownce")
+
+allowance = 2000
+
+allowance -= 400
+print(f"Account remain: {allowance}")
+
+allowance += 100
+print(f"added 100 naira: {allowance}")
+
+allowance -=250
+print(f"Bought snacks: {allowance}")
+
+allowance *= 0.75
+print(f"You gave 25% out: {allowance}")
+
+
+allowance = ((2/3) * allowance)
+print(f"You use one-third: {allowance}")
+
+allowance =  ((allowance) //2)
+print(f"split the remaining: {allowance}")
+
+allowance %= 100
+print(f"Finally: {allowance}")
+
